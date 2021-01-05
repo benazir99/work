48a7716efcc06dde333eaf1e878a1e3fff4ff2d6
unknown <sultonqulovabduhoshim@gmail.com>
2021-01-05 21:05:33 +0300

    first github,added and projekt

---
+++ main.js
@@ -0,0 +1,65 @@
+//var yosh = prompt('what is your age?');
+
+//switch(yosh){
+    //case '18':
+    //console.log('yoshingiz yetarli');
+    //break;
+    //case '40':
+    //console.log('sz juda qarisz');
+    //break;
+    //default:
+    //console.log("o'zing bilasan");
+    //break;
+//}
+
+
+//function myadmin(){
+  //  console.log('qwerty');
+///}
+//myadmin();
+//myadmin();
+//myadmin();
+
+//var les = 'Abduhoshim';
+//var uzb = "o'zbekiston";
+//var story = 'lorem ipsom.';
+//console.log(story.length);
+//console.log(story.indexOf('.').length);
+
+//function mytab(box,qwe,oiu){
+  //  console.log(box.length);
+   // console.log(box.index);
+//}
+///mytab('I love css','loi wdgeu','')
+
+//var a = 'string bola agcyu akjedkw bola kbfejb bola';
+///console.log(a.indexOf('t'));
+//console.log(a.charAt('3'));
+//console.log(a.repeat(100));
+///console.log(a.match(/bola/g).length);
+
+
+//function myPhoneNomer(){
+ //   var phoneCode = Math.floor(Math.random()*(100-90))+90;
+  //  var phoneNumber = Math.floor(Math.random()*(1000-100))+100;
+   // var phoneNumberLast = Math.floor(Math.random()*100).toString().padStart(2,'0');
+   // var phoneNumberLest = Math.floor(Math.random()*100).toString().padStart(2,'0');
+
+    //console.log('+998',phoneCode,phoneNumber,phoneNumberLast,phoneNumberLest);
+
+//}
+//myPhoneNomer()
+
+//var nav = Date();
+//console.log(nav);
+ 
+
+document.getElementById('class').addEventListener('click', displayDate);
+  
+function displayDate(){
+  
+}
+
+
+
+
---
+++ style.js
@@ -0,0 +1,15 @@
+//arr1 = [1,2,3,4];
+//arr2 = [5,6,7,8];
+//console.log(Math.min.apply(null,arr1));
+[1, 2].forEach(alert)
+
+
+
+var form = document.querySelector('form');
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+    var input = document.querySelector('#hello input');
+    input.value = 'PALONCHI';
+})
+
+
